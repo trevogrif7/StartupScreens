aefb99304b96f42882b5749953da9124b044f731
trevogrif7 <trevogrif7@gmail.com>
2016-12-17 14:34:38 -0600

    Initial Commit

---
+++ StartupScreens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StartupScreens
+//
+//  Created by Trevor Griffin on 12/17/16.
+//  Copyright © 2016 TREVOR E GRIFFIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
